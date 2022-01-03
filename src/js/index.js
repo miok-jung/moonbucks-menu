2a99b3fcd592a5fad64ca66e32c1ecdfd34c23e4
miok-jung <miok.dev@gmail.com>
2022-01-03 14:57:53 +0900

    feat: localStorage에 추가/수정할 때 저장하기

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,46 @@
 // [O] 메뉴의 수정 버튼을 눌러 메뉴 이름 수정할 수 있다.
 // [O] 메뉴 수정시 브라우저에서 제공하는 `prompt` 인터페이스를 활용한다.
 // [O] 자바스크립트 DOM Element를 가져올 때 달러표시를 관용적으로 많이 사용한다.
+
+// ANCHOR step2
+// TODO localStorage Read & Write
+// [2-1] localStorage에 데이터를 저장한다.
+// [2-1-1] 메뉴를 추가할 때 저장
+// [2-1-2] 메뉴를 수정할 때 저장
+// [2-1-3] 메뉴를 삭제할 때 저장
+// [2-2] localStorage에 있는 데이터를 읽어온다.
+
+// TODO 카테고리별 메뉴판 관리
+// [] 에스프레소 메뉴판을 관리
+// [] 프라푸치노 메뉴판을 관리
+// [] 블렌디드 메뉴판을 관리
+// [] 티바나 메뉴판을 관리
+// [] 디저트 메뉴판을 관리
+
+// TODO 페이지 접근시 최초 데이터 Read & Rendering
+// [] 페이지에 최초로 접근할 때는 localStorage에서 에스프레소 메뉴를 불러온다.
+// [] 에스프레소 메뉴를 페이지에 그려준다.
+
+// TODO 품절 상태 관리
+// [] 품절 상태인 경우를 보여줄 수 있게, 품절 버튼을 추가하고 sold-out class를 추가하여 상태를 변경한다.
+// [] 품절 상태 메뉴를 추가한다.
+// [] 품절 버튼을 클릭하면 localStorage에 상태값이 저장된다.
+// [] 클릭이벤트에서 가장 가까운 li 태그의 class 속성 값에 sold-out을 추가한다.
+
 const $ = (selector) => document.querySelector(selector);
+
+const store = {
+  setLocalStorage(menu) {
+    localStorage.setItem("menu", JSON.stringify(menu));
+  },
+  getLocalStorage() {
+    localStorage.getItem("menu");
+  },
+};
 function App() {
+  // 상태는 변하는 데이터, 이 앱에서 변하는 것이 무엇인가 - 갯수, 메뉴명
+  this.menu = [];
+
   const updateMenuCount = () => {
     const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
     $(".menu-count").innerText = `총 ${menuCount}개`;
@@ -14,25 +52,29 @@ function App() {
       return;
     }
     const espressoMenuName = $("#espresso-menu-name").value;
-    const menuItemTemplate = (espressoMenuName) => {
-      return ` 
-          <li class="menu-list-item d-flex items-center py-2">
-            <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
-            <button type="button" class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button">수정</button>
-            <button type="button" class="bg-gray-50 text-gray-500 text-sm menu-remove-button">삭제</button>
-          </li>`;
-    };
-    $("#espresso-menu-list").insertAdjacentHTML(
-      "beforeend",
-      menuItemTemplate(espressoMenuName)
-    );
+    this.menu.push({ name: espressoMenuName });
+    store.setLocalStorage(this.menu); // 2-1-1
+    const template = this.menu
+      .map((menuItem, index) => {
+        return `
+        <li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
+          <span class="w-100 pl-2 menu-name">${menuItem.name}</span>
+          <button type="button" class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button">수정</button>
+          <button type="button" class="bg-gray-50 text-gray-500 text-sm menu-remove-button">삭제</button>
+        </li>`;
+      })
+      .join("");
+    $("#espresso-menu-list").innerHTML = template;
     updateMenuCount();
     $("#espresso-menu-name").value = "";
   };
   const updateMenuName = (e) => {
+    const menuId = e.target.closest("li").dataset.menuId;
     const $menuName = e.target.closest("li").querySelector(".menu-name");
     //closest : 상위 부모 해당하는 태그값을 가져온다?
     const updatedMenuName = prompt("메뉴명을 수정하세요", $menuName.innerText);
+    this.menu[menuId].name = updatedMenuName;
+    store.setLocalStorage(this.menu); // 2-1-2
     $menuName.innerText = updatedMenuName;
   };
   const removeMenuName = (e) => {
@@ -75,5 +117,5 @@ function App() {
     addMenuName();
   });
 }
-App();
+const app = new App();
 // 구현으로 완료를 하게 되면 재사용함수와 따로 나누어 정리할 것
